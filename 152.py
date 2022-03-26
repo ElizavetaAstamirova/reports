9b7be5b88b7bdac0e1d5a2f9c80a9674b7e4f987
ElizavetaAstamirova <92157888+ElizavetaAstamirova@users.noreply.github.com>
2022-03-26 13:35:49 +0300

    Delete 152.py

--- 152.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-matrix = []
-for i in range(n):
-    matrix.append(list(map(int, input().split())))
-s = int(input())
-for i in range(n):
-    print(i)
-    matrix[s - 1][i] += 3
-print(matrix)
